135a018b8a83e32da0f5926a8459f96a4109d888
Christian Moreira <chmleave@gmail.com>
2020-05-14 03:04:59 -0400

    Add I18n pt-BR

--- spec/features/user_vew_home_page_spec.rb
+++ spec/features/user_vew_home_page_spec.rb
@@ -6,5 +6,8 @@ feature 'User view home page' do
 
     expect(page).to have_content('Headhunter Web')
     expect(page).to have_content('Seja bem-vindo')
+    expect(page).to have_link('Inicio', href: root_path)
+    expect(page).to have_link('Entrar', href: new_user_session_path)
+    expect(page).to have_link('Cadastrar-me', href: new_user_registration_path)
   end
 end
